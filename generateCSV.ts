1ad3b572a6e0fac62ff5734de808ec2d5866f5a5
Tasos Tsournos <tasos@moveo.ai>
2024-07-02 19:13:42 +0300

    fix argentninan nums

--- generateCSV.ts
+++ generateCSV.ts
@@ -1,12 +1,17 @@
 import * as fs from 'fs';
 import { faker } from '@faker-js/faker';
 
+const randomNumber = (min: number, max: number): number => {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+
+}
+
 const generateAndExportCSV = (): void => {
     const header = 'external_id,user.display_name,user.email\n';
     let csvContent = header;
 
-    for (let i = 0; i < 10; i++) {
-        const externalId = '+' + faker.phone.number('54911########');
+    for (let i = 0; i < 1000; i++) {
+        const externalId = '+' + faker.phone.number(`54911${randomNumber(3,8)}#######`);
         const displayName = faker.name.firstName();
         const email = faker.internet.email(displayName.toLowerCase());
 
